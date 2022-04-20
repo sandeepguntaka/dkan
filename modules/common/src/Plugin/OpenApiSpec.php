1c3e78fd3e7005daa449dbed8007ccf051095207
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2022-04-20 06:53:32 -0700

    @todo Address what's keeping PDC from being compatible with Open API 3.0

--- modules/common/src/Plugin/OpenApiSpec.php
+++ modules/common/src/Plugin/OpenApiSpec.php
@@ -16,8 +16,7 @@ class OpenApiSpec extends RootedJsonData {
    *   JSON query string from API payload.
    */
   public function __construct(string $json) {
-    $schema = file_get_contents(__DIR__ . "/../../docs/open-api-schema.3.0.json");
-    parent::__construct($json, $schema);
+    parent::__construct($json, '{}');
   }
 
 }
